98b149e74349106e3571b37aa95ae10a1883797e
Richard Lim <mango__82@hotmail.com>
2022-09-24 16:12:43 +1000

    add comments

--- script.js
+++ script.js
@@ -7,27 +7,32 @@ const operatorButtons = document.querySelectorAll(".operator")
 const equalButton = document.getElementById("equal-sign")
 const decimalButton = document.getElementById("decimal")
 
+//Function to add numbers to display when pressing buttons
 for (let i = 0; i < numberButtons.length; i++){
     numberButtons[i].addEventListener("click", e => {
         currentOperand.innerHTML += numberButtons[i].innerHTML
     })
 }
 
+//Function to only add 1 decimal point to display
 decimalButton.addEventListener("click", e => {
     if (currentOperand.innerHTML.includes(".") === false){
         currentOperand.innerHTML += decimalButton.innerHTML
     }
 })
 
+//Function to clear data when clicked
 clearButton.addEventListener("click", e => {
     currentOperand.innerHTML = ""
     previousOperand.innerHTML = ""
 })
 
+//Function to delete the most recent item added to display
 deleteButton.addEventListener("click", e => {
     currentOperand.innerHTML = currentOperand.innerHTML.slice(0, -1)
 })
 
+//Function to put first number and operator into the second display
 for (let i = 0; i < operatorButtons.length; i++) {
     operatorButtons[i].addEventListener("click", e => {
         if (previousOperand.innerHTML === ""){
@@ -40,6 +45,7 @@ for (let i = 0; i < operatorButtons.length; i++) {
     })
 }
 
+//Function that will run the operate function when clicked
 equalButton.addEventListener("click", e => {
     console.log(operate())
 })
@@ -60,6 +66,7 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
+//Operate function which gets operator and two numbers using the innerHTML display values and returns result
 function operate(operator){
     operator = previousOperand.innerHTML.charAt(previousOperand.innerHTML.length - 1)
     num1 = previousOperand.innerHTML.slice(0, -1)
