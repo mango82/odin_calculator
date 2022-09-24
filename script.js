8092e9a9c726387e6f098caf877ee629fb101008
Richard Lim <mango__82@hotmail.com>
2022-09-24 13:39:37 +1000

    number, delete, and clear functionality

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const previousOperand = document.querySelector("#previous-operand")
+const currentOperand = document.querySelector("#current-operand")
+const numberButtons = document.querySelectorAll(".number")
+const clearButton = document.getElementById("clear")
+const deleteButton = document.getElementById("delete")
+
+for (let i = 0; i < numberButtons.length; i++){
+    numberButtons[i].addEventListener("click", e => {
+        currentOperand.innerHTML += numberButtons[i].innerHTML
+    })
+}
+
+clearButton.addEventListener("click", e => {
+    currentOperand.innerHTML = ""
+})
+
+deleteButton.addEventListener("click", e => {
+    currentOperand.innerHTML = currentOperand.innerHTML.slice(0, -1)
+})
+
 function add(num1, num2) {
     return num1 + num2
 }
